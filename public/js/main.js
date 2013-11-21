e97bb128f5d27125f42fb3345aa591288a897aa2
sully.rafiq <sully.rafiq@moneysupermarket.com>
2013-11-21 13:20:47 +0000

    Starting to build out JS framework. Using handlebars

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,20 @@
-var AppView = Backbone.View.extend({
-      // el - stands for element. Every view has a element associate in with HTML 
-      //      content will be rendered.
-      el: '#search-container',
-      template: _.template("<h2><%= Title %></h2><input type='text' id='vehicle_reg'><button id='search'>Search</button>"),
-      initialize: function(){
-        this.render();
-      },
-      // $el - it's a cached jQuery object (el), in which you can use jQuery functions 
-      //       to push content. Like the Hello World in this case.
-      render: function(){
-        this.$el.html(this.template({Title: 'SlickFit Tyres'}));
-      }
-    });
-
 $(document).ready(function() {
-	var appView = new AppView();
+	
+  $("#search").click(function(e) {
+
+    e.preventDefault();
+
+    $.getJSON('/basic', function(data) {      
+      var source   = $("#basic-template").html();
+      var template = Handlebars.compile(source);
+      $("#tyre-prices-container").html(template(data));
+    })
+    .done(function(){
+          
+    })
+    .fail(function(error) {
+      alert(error);
+    });
+  })  
+
 });
--- web.rb
+++ web.rb
@@ -6,6 +6,13 @@ get '/' do
     erb :home
 end
 
+get '/basic' do 
+	"{
+    	\"make\": \"audi\",
+    	\"model\": \"a3\" 
+	}"
+end
+
 post "/update_repos" do
   Process.detach(fork{ exec "/home/ec2-user/update-repos.sh &"})
   "received repo update message"
