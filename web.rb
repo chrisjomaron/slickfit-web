246eebf45274860062a30f5e198e73e0d13767fc
Thomas Dittmer <thomas.dittmer@moneysupermarket.com>
2013-11-21 19:59:45 +0000

    greig's first commit -added routing for the web to the API

--- web.rb
+++ web.rb
@@ -5,18 +5,71 @@ require 'shotgun'
 require 'json'
 require 'httpclient'
 
+before do
+	@http = HTTPClient.new
+end
+
 get '/' do
     erb :home
+end
+
+get '/servicing/:reg/:postcode' do 
+	content_type :json
+
+	response = @http.get "http://localhost:4987/servicing/#{params[:reg]}/#{params[:postcode]}"
+	response.body
+end
+
+get '/car-details/:reg' do 
+	content_type :json
+
+	response = @http.get "http://localhost:4987/car-details/#{params[:reg]}"
+	response.body
+end
+
+get '/fitting-stations/:postcode' do 
+	content_type :json
+
+	response = @http.get "http://localhost:4987/fitting-stations/#{params[:postcode]}"
+	response.body
 end
 
 get '/tyre-prices/:reg' do 
 	content_type :json
 
-	http = HTTPClient.new
-	response = http.get "http://localhost:4987/tyre-prices/#{params[:reg]}"
+	response = @http.get "http://localhost:4987/tyre-prices/#{params[:reg]}"
 	response.body
-	# status, headers, body = call env.merge("PATH_INFO" => "http://localhost:4987/tyre-prices/#{params[:reg]}")
-  	# [status, headers, body.map(&:upcase)]
+end
+
+get '/tyre-sizes/:reg' do 
+	content_type :json
+
+	response = @http.get "http://localhost:4987/tyre-sizes/#{params[:reg]}"
+	response.body
+end
+
+get '/break-down-cover/:reg' do 
+	content_type :json
+
+	response = @http.get "http://localhost:4987/break-down-cover/#{params[:reg]}"
+	response.body
+end
+
+get '/mot/:postcode/?:distance?/?:num_of_results?' do 
+	content_type :json
+
+	url = "http://localhost:4987/mot/#{params[:postcode]}"
+
+	unless params[:distance].nil?
+		url = "#{url}/#{params[:distance]}"
+	end
+
+	unless params[:num_of_results].nil?
+		url = "#{url}/#{params[:num_of_results]}"
+	end 
+
+	response = @http.get url
+	response.body
 end
 
 # 
